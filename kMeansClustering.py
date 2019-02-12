6fbdfc333b44c27da7d23538bacbd85588a8ee18
NamanCMU <naman.kumar.1991@gmail.com>
2019-02-12 18:39:02 +0530

    First working version

--- kMeansClustering.py
+++ kMeansClustering.py
@@ -23,38 +23,53 @@ for file in os.listdir(folderName):
 		img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
 		allImages.append(img2)
 
-print("Total Images: ", len(allImages))	
+print("Total Images: ", len(allImages), ' ,', len(allFiles))	
 
-# Gaussian Blur and Otsu Thresholding
-imgOne = np.array(allImages[0])
-imgBlur = cv2.GaussianBlur(imgOne,(5,5),0)
-ret, imgOneThresh = cv2.threshold(imgBlur,0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+count = 0
+for inputImg in allImages:
+	print("Processing Image ", count + 1);
+	imgOne = np.array(inputImg)
+	# Gaussian Blur and Otsu Thresholding
+	imgBlur = cv2.GaussianBlur(imgOne,(5,5),0)
+	ret, imgOneThresh = cv2.threshold(imgBlur,0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-imgOneFlat = imgOneThresh.reshape(imgOneThresh.shape[0] * imgOneThresh.shape[1], 1) # Reshape into a column vector
+	imgOneFlat = imgOneThresh.reshape(imgOneThresh.shape[0] * imgOneThresh.shape[1], 1) # Reshape into a column vector
 
-#Using X, Y, Grayscale value as features
-Coord = np.zeros((imgOne.shape[0] * imgOne.shape[1], 1))
-for i in range(0,Coord.shape[0]):
-	Coord[i][0] = i
+	#Using X, Y, Grayscale value as features
+	Coord = np.zeros((imgOne.shape[0] * imgOne.shape[1], 1))
+	for i in range(0,Coord.shape[0]):
+		Coord[i][0] = i
 
-finalVec = np.hstack((imgOneFlat, Coord))
-finalVec = np.float32(finalVec)
+	finalVec = np.hstack((imgOneFlat, Coord))
+	finalVec = np.float32(finalVec)
 
-# using K-means clustering
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-compactness,labels,centers = cv2.kmeans(finalVec,3,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+	# using K-means clustering
+	criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10,1)
+	compactness,labels,centers = cv2.kmeans(finalVec,3,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
 
-# Three clusters (Top, Document, Bottom)
-first = finalVec[labels.ravel()==0]
-second = finalVec[labels.ravel()==1]
-third = finalVec[labels.ravel()==2]
+	# Three clusters
+	first = finalVec[labels.ravel() == 0]
+	second = finalVec[labels.ravel() == 1]
+	third = finalVec[labels.ravel() == 2]
 
-print("Final Vec size: " , finalVec.shape)
+	for index in first[:,1]:
+		imgOneFlat[int(index)][0] = 255
 
-# Plotting the data
-plt.scatter(first[:,0],first[:,1])
-plt.scatter(second[:,0],second[:,1])
-plt.scatter(third[:,0],third[:,1])
-plt.scatter(centers[:,0],centers[:,1],s = 50,c = 'r', marker = 's')
-plt.xlabel('GrayValue'),plt.ylabel('Coordinate')
-plt.show()
+	croppedImg = imgOneFlat.reshape(imgOneThresh.shape[0], imgOneThresh.shape[1]) & imgOne
+
+	# Saving the image
+	name, ext = os.path.splitext(allFiles[count])
+	fileName = name + 'cropped.jpg'
+
+	cv2.imwrite(os.path.join(folderName , fileName), croppedImg)
+	count = count + 1
+
+	# Plotting the data
+	# plt.subplot(111), plt.imshow(croppedImg, 'gray')
+
+	plt.scatter(first[:,1],first[:,0], c = 'g')
+	plt.scatter(second[:,1],second[:,0], c = 'b')
+	plt.scatter(third[:,1],third[:,0], c = 'k')
+	plt.scatter(centers[:,1],centers[:,0],s = 50,c = 'r', marker = 's')
+	plt.xlabel('Coordinate'),plt.ylabel('GrayValue')
+	plt.show()
