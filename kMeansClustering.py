5c75e6f93442e78bbf85e6687660c06c8f2690ee
NamanCMU <naman.kumar.1991@gmail.com>
2019-02-12 19:00:42 +0530

    Working K-means implementation

--- kMeansClustering.py
+++ kMeansClustering.py
@@ -23,12 +23,14 @@ for file in os.listdir(folderName):
 		img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
 		allImages.append(img2)
 
-print("Total Images: ", len(allImages), ' ,', len(allFiles))	
+print("Total Images: ", len(allImages))	
 
 count = 0
 for inputImg in allImages:
+
 	print("Processing Image ", count + 1);
 	imgOne = np.array(inputImg)
+	
 	# Gaussian Blur and Otsu Thresholding
 	imgBlur = cv2.GaussianBlur(imgOne,(5,5),0)
 	ret, imgOneThresh = cv2.threshold(imgBlur,0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)
@@ -48,28 +50,41 @@ for inputImg in allImages:
 	compactness,labels,centers = cv2.kmeans(finalVec,3,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
 
 	# Three clusters
-	first = finalVec[labels.ravel() == 0]
-	second = finalVec[labels.ravel() == 1]
-	third = finalVec[labels.ravel() == 2]
+	first = finalVec[labels.flatten() == 0]
+	second = finalVec[labels.flatten() == 1]
+	third = finalVec[labels.flatten() == 2]
 
-	for index in first[:,1]:
-		imgOneFlat[int(index)][0] = 255
+	# finding which label corresponds to the document
+	# Assumption: cluster center with highest gray value (closer to white) is the correct cluster
+	graycenter = np.array([centers[0][0], centers[1][0], centers[2][0]])
 
-	croppedImg = imgOneFlat.reshape(imgOneThresh.shape[0], imgOneThresh.shape[1]) & imgOne
+	if(graycenter.argmax(axis = 0) == 0):
+		contentLabels = first;
+	if(graycenter.argmax(axis = 0) == 1):
+		contentLabels = second;
+	if(graycenter.argmax(axis = 0) == 2):
+		contentLabels = third;
+
+	# Filling the document region with white
+	for index in contentLabels[:,1]:
+		imgOneFlat[int(index)][0] = 255 
+
+	croppedImg = imgOneFlat.reshape(imgOneThresh.shape[0], imgOneThresh.shape[1]) & imgOne # Reshaping and then ANDing with the original image
 
 	# Saving the image
 	name, ext = os.path.splitext(allFiles[count])
 	fileName = name + 'cropped.jpg'
 
 	cv2.imwrite(os.path.join(folderName , fileName), croppedImg)
-	count = count + 1
 
 	# Plotting the data
 	# plt.subplot(111), plt.imshow(croppedImg, 'gray')
 
-	plt.scatter(first[:,1],first[:,0], c = 'g')
-	plt.scatter(second[:,1],second[:,0], c = 'b')
-	plt.scatter(third[:,1],third[:,0], c = 'k')
-	plt.scatter(centers[:,1],centers[:,0],s = 50,c = 'r', marker = 's')
-	plt.xlabel('Coordinate'),plt.ylabel('GrayValue')
-	plt.show()
+	# plt.scatter(first[:,1],first[:,0], c = 'g')
+	# plt.scatter(second[:,1],second[:,0], c = 'b')
+	# plt.scatter(third[:,1],third[:,0], c = 'k')
+	# plt.scatter(centers[:,1],centers[:,0],s = 50,c = 'r', marker = 's')
+	# plt.xlabel('Coordinate'),plt.ylabel('GrayValue')
+	# plt.show()
+	
+	count = count + 1
